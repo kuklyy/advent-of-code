cf33163c5744f9c9f2a7ee6fc3bb1a24d42fdfd9
Jakub Mikłasz <jakubmiklasz9@gmail.com>
2021-12-08 00:09:55 +0100

    day6p2

--- 6-lanternfish/main.go
+++ 6-lanternfish/main.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	part1()
+	part2()
 }
 
 // 387413
@@ -42,7 +43,6 @@ func part1() {
 	fishInterval := 6
 
 	for i := 0; i < days; i++ {
-		fmt.Println(i)
 		newbornFish := make([]int, 0, len(fishFamily))
 
 		for i := range fishFamily {
@@ -57,4 +57,51 @@ func part1() {
 	}
 
 	fmt.Println(len(fishFamily))
+}
+
+// 1738377086345
+func part2() {
+	scanner, file := common.ReadFile("./input.txt")
+
+	defer file.Close()
+
+	table := make([]int, 9)
+
+	var input string
+
+	for scanner.Scan() {
+		input = scanner.Text()
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, strV := range strings.Split(input, ",") {
+		v, _ := strconv.Atoi(strV)
+		table[v]++
+	}
+
+	for i := 0; i < 256; i++ {
+		dayTable := make([]int, 9)
+
+		for j := len(table) - 1; j >= 0; j-- {
+
+			if j == 0 {
+				dayTable[len(table)-1] = table[0]
+				dayTable[6] = dayTable[6] + table[0]
+				continue
+			}
+			dayTable[j-1] = table[j]
+		}
+		table = dayTable
+	}
+
+	sum := 0
+
+	for _, v := range table {
+		sum += v
+	}
+
+	fmt.Println(sum)
 }
