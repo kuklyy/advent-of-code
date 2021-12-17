f1d478629c2ef8ec5fd79fc83664560b9d05600d
Jakub Mikłasz <jakubmiklasz9@gmail.com>
2021-12-17 17:27:14 +0100

    day 14

--- 14-extended-polymerization/main.go
+++ 14-extended-polymerization/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
 
 	"github.com/kuklyy/advent-of-code-2021/common"
@@ -9,13 +10,15 @@ import (
 
 func main() {
 	part1()
+	part2()
 }
 
+// 2967
 func part1() {
 	template := make([]string, 0, 4096)
 	insertionRules := make([][]string, 0, 128)
 
-	scanner, input := common.ReadFile("sample.txt")
+	scanner, input := common.ReadFile("input.txt")
 
 	defer input.Close()
 
@@ -40,9 +43,9 @@ func part1() {
 	step := 10
 
 	for i := 0; i < step; i++ {
-		stepTemplate := make(map[int][]string)
+		stepTemplate := make([][]string, 0, len(template))
 		for j := 0; j < len(template)-1; j++ {
-			stepTemplate[j] = []string{fmt.Sprintf("%s%s", template[j], template[j+1]), ""}
+			stepTemplate = append(stepTemplate, []string{fmt.Sprintf("%s%s", template[j], template[j+1]), ""})
 		}
 
 		for _, rule := range insertionRules {
@@ -62,12 +65,8 @@ func part1() {
 			}
 			newTemplate = append(newTemplate, string(k[0][1]))
 		}
-
 		template = newTemplate
-		fmt.Println(template)
 	}
-
-	fmt.Println(len(template))
 
 	countMap := make(map[string]int)
 
@@ -78,5 +77,98 @@ func part1() {
 		countMap[v] += 1
 	}
 
-	fmt.Println(countMap)
+	maxValue := math.MinInt64
+	minValue := math.MaxInt64
+
+	for _, v := range countMap {
+		if v > maxValue {
+			maxValue = v
+		}
+
+		if v < minValue {
+			minValue = v
+		}
+	}
+
+	fmt.Println(maxValue - minValue)
+}
+
+func part2() {
+	pairMap := make(map[string]int)
+	insertionRules := make([][]string, 0, 128)
+
+	scanner, input := common.ReadFile("input.txt")
+
+	defer input.Close()
+
+	readRules := false
+
+	countMap := make(map[string]int)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			readRules = true
+			continue
+		}
+
+		if !readRules {
+			data := strings.Split(line, "")
+			for i := 0; i < len(data)-1; i++ {
+				key := fmt.Sprintf("%s%s", data[i], data[i+1])
+				appendPair(pairMap, 1, key)
+				if i == len(data)-2 {
+					appendPair(countMap, 1, data[i+1])
+				}
+			}
+		} else {
+			parsedRule := strings.Split(line, " -> ")
+			insertionRules = append(insertionRules, []string{parsedRule[0], parsedRule[1]})
+		}
+	}
+
+	step := 40
+
+	for i := 0; i < step; i++ {
+		stepPairMap := make(map[string]int)
+
+		for _, rule := range insertionRules {
+			for pair, count := range pairMap {
+				if pair == rule[0] {
+					appendPair(stepPairMap, count, fmt.Sprintf("%s%s", string(pair[0]), rule[1]))
+					appendPair(stepPairMap, count, fmt.Sprintf("%s%s", rule[1], string(pair[1])))
+				}
+			}
+		}
+
+		pairMap = stepPairMap
+	}
+
+	for k, v := range pairMap {
+		appendPair(countMap, v, string(k[0]))
+	}
+
+	maxValue := math.MinInt64
+	minValue := math.MaxInt64
+
+	for _, v := range countMap {
+		if v > maxValue {
+			maxValue = v
+		}
+
+		if v < minValue {
+			minValue = v
+		}
+	}
+
+	fmt.Println(maxValue - minValue)
+
+}
+
+func appendPair(pairMap map[string]int, count int, key string) {
+	if _, ok := pairMap[key]; !ok {
+		pairMap[key] = 0
+	}
+	pairMap[key] += count
 }
